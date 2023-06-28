457b68c7f6537e7a74ca2d29d5ee72bc7b5d8501
DyhaCreator <andropet228@gmail.com>
2023-06-28 09:25:58 -0400

    create new branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# eaters
-this is a bio-cybernetic model that should repeat or at least simulate real creatures
--- creatures.py
+++
@@ -1,8 +0,0 @@
-import VectorsLib as vec
-import random
-
-class Creatures:
-    def __init__(self,SIZE_WORLD):
-        self.x = random.randint(0,SIZE_WORLD)
-        self.eat = 100
-        self.speed = 0
